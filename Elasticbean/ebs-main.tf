cc4a5d5c7a1ac874794365e61fd7e24ee64ab508
Deepayan Kundu <deepayankundu6@gmail.com>
2023-11-08 17:48:05 +0530

    dvdv

--- Elasticbean/ebs-main.tf
+++ Elasticbean/ebs-main.tf
@@ -1,52 +1,52 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
 }
 
-resource "aws_elastic_beanstalk_application" "example" { 
- name = "my-nodejs-app" 
+data "archive_file" "example" {
+  type        = "zip"
+  source_dir  = "express-app/"
+  output_path = "express-app.zip"
 }
- 
-resource "aws_elastic_beanstalk_environment" "example" { 
- name = "deeps-nodejs-environment"
- application = aws_elastic_beanstalk_application.example.name 
- solution_stack_name = "64bit Amazon Linux 2 v5.4.1 running Node.js 18" 
-} 
+resource "aws_s3_bucket" "default" {
+  bucket = "deep-241196-node-app"
+}
 
-data "archive_file" "example" { 
- type = "zip" 
- source_dir = "example-app/" 
- output_path = "example-app.zip" 
-} 
+resource "aws_s3_object" "default" {
+  bucket = aws_s3_bucket.default.id
+  key    = "beanstalk/express-app.zip"
+  source = "express-app.zip"
+}
+
+resource "aws_elastic_beanstalk_application" "example" {
+  name = "my-nodejs-app"
+}
 
 resource "aws_elastic_beanstalk_application_version" "example" {
- name = "example-app-version" 
- bucket = ""
- key = ""
- application = aws_elastic_beanstalk_application.example.name 
- description = "Example Node.js Application"
- source_bundle = "example-app.zip" 
+  name        = aws_elastic_beanstalk_application.example.name
+  bucket      = aws_s3_bucket.default.id
+  key         = aws_s3_object.default.id
+  application = aws_elastic_beanstalk_application.example.name
+  description = "Example Node.js Application"
 }
- 
+
 resource "aws_elastic_beanstalk_environment" "example" {
- name = "example-nodejs-environment"
- application = aws_elastic_beanstalk_application.example.name
- solution_stack_name = "64bit Amazon Linux 2 v5.4.1 running Node.js 18"
- setting { 
+  name                = "deeps-nodejs-environment"
+  application         = aws_elastic_beanstalk_application.example.name
+  solution_stack_name = "64bit Amazon Linux 2023 v6.0.2 running Node.js 18"
+  setting {
     namespace = "aws:elasticbeanstalk:container:nodejs"
-    name = "NodeCommand" value = "npm start"
- } 
- setting { 
-     namespace = "aws:elasticbeanstalk:environment" 
-     name = "PORT" value = "3000" 
- } 
- setting {
-     namespace = "aws:elasticbeanstalk:application:environment" 
-     name = "NODE_ENV" value = "production" 
- } 
- setting {
-     namespace = "aws:elasticbeanstalk:application:environment" 
-     name = "EXAMPLE_APP_DB_URI" 
-     value = "mongodb://example-user:example-password@example-db.example-region.rds.amazonaws.com:27017/example-db" 
-} 
-depends_on = [ aws_elastic_beanstalk_application_version.example ] 
+    name      = "NodeCommand"
+    value     = "npm start"
+  }
+#   setting {
+#     namespace = "aws:elasticbeanstalk:environment"
+#     name      = "PORT"
+#     value     = "3000"
+#   }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "NODE_ENV"
+    value     = "production"
+  }
+  depends_on = [aws_elastic_beanstalk_application_version.example]
 } 
