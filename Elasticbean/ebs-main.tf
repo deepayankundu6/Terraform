f437afec4bf4849ab932048772ccf684045f0063
Deepayan Kundu <deepayankundu6@gmail.com>
2023-11-08 18:31:57 +0530

    xvv

--- Elasticbean/ebs-main.tf
+++ Elasticbean/ebs-main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "my-jnode-eb-lg"
+}
+
 data "archive_file" "example" {
   type        = "zip"
   source_dir  = "express-app/"
@@ -33,10 +37,32 @@ resource "aws_elastic_beanstalk_environment" "example" {
   name                = "deeps-nodejs-environment"
   application         = aws_elastic_beanstalk_application.example.name
   solution_stack_name = "64bit Amazon Linux 2023 v6.0.2 running Node.js 18"
+  tier                = "WebServer"
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "NODE_ENV"
     value     = "production"
   }
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
+    name      = "StreamLogs"
+    value     = true
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance"
+  }
+  setting {
+    namespace = "aws:elb:listener:listener_port"
+    name      = "ListenerProtocol"
+    value     = "HTTP"
+  }
+  
   depends_on = [aws_elastic_beanstalk_application_version.example]
-} 
+}
